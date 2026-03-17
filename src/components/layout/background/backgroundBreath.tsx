9c93622721f5392bef26644da0c28902004dd720
João Victor Ventura Martins <jvmartinscv@gmail.com>
2026-03-17 18:22:01 -0300

    refactor: remove arquivos antigos

--- src/components/layout/background/backgroundBreath.tsx
+++
@@ -1,7 +0,0 @@
-export function BackgroundBreath() {
-  return (
-    <div className="pointer-events-none absolute inset-0 -z-10 flex items-center justify-center overflow-hidden">
-      <div className="breath-gradient" />
-    </div>
-  )
-}
