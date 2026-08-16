fd9a2b2d4208f425cbb7e7c8369517b54cf5098c
João Victor Ventura Martins <jvmartinscv@gmail.com>
2026-08-15 23:51:35 -0300

    refactor(gamedev): move secao de gamedev da home para a pagina sobre

--- src/routes/_site/index.tsx
+++ src/routes/_site/index.tsx
@@ -2,7 +2,6 @@ import { createFileRoute } from "@tanstack/react-router";
 import { IntroSection } from "@/features/home/components/IntroSection";
 import { HighlightsSection } from "@/features/home/components/HighlightsSection";
 import { NowSection } from "@/features/home/components/NowSection";
-import { GameDevSection } from "@/features/games/components/GameDevSection";
 import { ConnectSection } from "@/features/home/components/ConnectSection";
 import { siteConfig } from "@/shared/config/site";
 import { pageHead } from "@/shared/lib/seo";
@@ -18,7 +17,6 @@ function HomePage() {
       <IntroSection />
       <HighlightsSection />
       <NowSection />
-      <GameDevSection />
       <ConnectSection />
     </>
   );
